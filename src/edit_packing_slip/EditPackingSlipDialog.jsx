ed6d05e73d53b84ab0d5ff9ffb1d058a729b2a13
Josh Plumley <plumleyj@gmail.com>
2022-08-11 21:17:53 -0400

    added shipped or staged

--- src/edit_packing_slip/EditPackingSlipDialog.jsx
+++ src/edit_packing_slip/EditPackingSlipDialog.jsx
@@ -1,7 +1,8 @@
 import PackingDialog from "../components/PackingDialog";
 import EditPackingSlipTable from "./components/EditPackingSlipTable";
-import { DialogActions } from "@mui/material";
+import { DialogActions, Grid, Typography } from "@mui/material";
 import DestinationToggle from "../packing_slip/components/DestinationToggle";
+import { fontWeight } from "@mui/system";
 
 const EditPackingSlipDialog = ({
   packingSlipData,
@@ -16,6 +17,7 @@ const EditPackingSlipDialog = ({
   cellEditing = false,
   viewOnly = true,
 }) => {
+  console.log(packingSlipData);
   return (
     <PackingDialog
       open={isOpen}
@@ -40,6 +42,13 @@ const EditPackingSlipDialog = ({
         onPackQtyChange={onPackQtyChange}
         viewOnly={viewOnly}
       />
+      <Grid container justifyContent="end">
+        {packingSlipData.shipment ? (
+          <Typography fontWeight="bold">SHIPPED</Typography>
+        ) : (
+          <Typography fontWeight="bold">STAGED</Typography>
+        )}
+      </Grid>
     </PackingDialog>
   );
 };
