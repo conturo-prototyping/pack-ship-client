01475ac0c4de05e646155fa7c7e2212d1f195fc4
jarrilla <jose@conturoprototyping.com>
2023-04-24 10:39:35 -0400

    conditional chain on downloadUrl to not brick FE render

--- src/packing_queue/utils/historyDetails.js
+++ src/packing_queue/utils/historyDetails.js
@@ -13,7 +13,7 @@ export const extractHistoryDetails = (history) => {
           ...item,
           item: {
             ...item.item,
-            routerUploadReady: item.item.downloadUrl !== undefined,
+            routerUploadReady: item?.item?.downloadUrl !== undefined,
           },
         };
       }),
